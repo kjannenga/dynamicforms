804d65a62b38b9da35303d291534adaa54af3542
KrystineJannenga <kjannenga1@gmail.com>
2017-03-22 08:52:54 -0700

    wednesday push

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
 $(document).ready(function(){
-	$.getJSON("http://json-data.herokuapp.com/forms" ,function(formData){
+	$.getJSON("http://json-data.herokuapp.com/forms" ,function(data){
+	var htmlStr = ""
 
-
-var htmlStr = ''
-
-data.forEach(function(field){
-	if (field.type === 'text' || field.type = 'email' || field.type === 'tel'){
-		htmlStr += `<i class ="fa ${field.icon}"" aria-hidde ="true"></i>
-		<input type="${field.type}" placeholder="${field.label}" id="$field.id}" />` 
+	data.forEach(function(field){
+		if (field.type === 'text' || field.type === 'email' || field.type === 'tel') {
+		htmlStr += `<div class=field"><i class ="fa ${field.icon}" aria-hidden ="true"></i>
+		<input type="${field.type}" placeholder="${field.label}" id="${field.id}" /></div>` 
 	}
 
-	if (field.type === 'select'){
-		htmlStr += `<select id=${field.id}"">
-			<option value =''>${field.label}</option> `
+		if (field.type === 'select'){
+			htmlStr += `<select id="${field.id}">
+				<option value =''>${field.label}</option>`
 
-			field.options.forEach(function(option){
-				htmlStr += `<option value` 
-			})
+				field.options.forEach(function(option){
+					htmlStr += `<option value="${option.value}">${option.label}</option>` 
+				})
+
+			htmlStr +=`</select>`
 
 	}
+
 	if (field.type === 'textarea'){
+		htmlStr += `<div class="field><i class ="fa ${field.icon}" aria-hidden ="true"></i><textarea id="${field.id}" placeholder="${field.label}"></textarea></div>`
 
 	}
-	
-})
+	})
 
+	$("#app").html(htmlStr)
 
+	$("input").on('focus', function(){
+		$(this).parents('div').addClass('focused')
+	}).on("blur", function(){
+		$(this).parents('div').removeClass('focused')
+	})
 
 	})
 })
@@ -36,4 +43,3 @@ data.forEach(function(field){
 
 
 
-formHTML += `<input class = "fa ${field.icon} />  <input type = "${field.type}" placeholder=${field.label} id="${field.id}"`
